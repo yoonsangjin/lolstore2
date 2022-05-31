8bfd8fc58bc2e3891a12eccd9ab54c226834443b
Hakseong-Lee <83964261+Hakseong-Lee@users.noreply.github.com>
2022-06-01 00:27:28 +0900

    first commit

--- src/routers/views-router.js
+++ src/routers/views-router.js
@@ -7,8 +7,10 @@ const viewsRouter = express.Router();
 // 아래와 같이 하면, http://localhost:5000/ 에서는 views/home/home.html 파일을,
 // http://localhost:5000/register 에서는 views/register/register.html 파일을 화면에 띄움
 viewsRouter.use('/', serveStatic('home'));
-viewsRouter.use('/register', serveStatic('register'));
+viewsRouter.use('/register', serveStatic('/register'));
 viewsRouter.use('/login', serveStatic('login'));
+// 계정관리에 들어가는걸 보기 위해 임시로 경로 설정
+viewsRouter.use('/account', serveStatic('account'));
 
 // views 폴더의 최상단 파일인 rabbit.png, api.js 등을 쓸 수 있게 함
 viewsRouter.use('/', serveStatic(''));
@@ -16,11 +18,11 @@ viewsRouter.use('/', serveStatic(''));
 // views폴더 내의 ${resource} 폴더 내의 모든 파일을 웹에 띄우며,
 // 이 때 ${resource}.html 을 기본 파일로 설정함.
 function serveStatic(resource) {
-  const resourcePath = path.join(__dirname, `../views/${resource}`);
-  const option = { index: `${resource}.html` };
+	const resourcePath = path.join(__dirname, `../views/${resource}`);
+	const option = { index: `${resource}.html` };
 
-  // express.static 은 express 가 기본으로 제공하는 함수임
-  return express.static(resourcePath, option);
+	// express.static 은 express 가 기본으로 제공하는 함수임
+	return express.static(resourcePath, option);
 }
 
 export { viewsRouter };
