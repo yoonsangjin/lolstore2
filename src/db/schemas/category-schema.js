9de9e71c503932839666ce115b5027aaae8e28a5
정승우 <sunjw0531@gmail.com>
2022-06-08 11:34:56 +0900

    카테고리, 상품 삭제 수정

--- src/db/schemas/category-schema.js
+++ src/db/schemas/category-schema.js
@@ -2,18 +2,13 @@ import { Schema } from 'mongoose';
 
 const CategorySchema = new Schema(
 	{
-		name: { type: String, required: true },
+		name: { type: String, required: true, unique: true },
 		products: [
 			{
 				type: Schema.Types.ObjectId,
 				ref: 'products',
 			},
 		],
-		// 0 : 사용중인 데이터 , 1 : 사용하지 않는 데이터
-		deleteFlag: {
-			type: Number,
-			default: 0,
-		},
 	},
 	{
 		collection: 'categories',
